6ac0fda13605f2960acc6ed3a114f6aebc74873a
sivanesanvelusamy95-prog <sivanesanvelusamy95@gmail.com>
2026-04-29 17:15:55 +0530

    class2

--- test.py
+++ test.py
@@ -1,2 +1,63 @@
 print("Welcome! Sivi")
 print("Welcome! Pythonday1")
+# first_name = "Guna"
+# age = 8
+
+# print(type(age))
+
+# age = "Eight"
+
+# print(type(age))
+
+# print(first_name, str(age), first_name, age)
+
+
+# a = b = c = 1001
+
+# a, b, c, d = 1001, 1002, 1003, 1004
+
+# print(a, b, c)
+
+# # if age > 18:
+# #     print("You are eligible to vote")
+
+
+# print(type(first_name))
+# print(type(age))
+
+# print("My name is " + first_name)
+
+
+# print("Hello " + first_name + " how r you")
+
+
+# print("Its me " + first_name)
+
+# print("Age of " + first_name + " is " + str(age))
+
+
+
+
+# data Types in Python
+
+# int, float, str, bool
+
+# int = 1001, 1, 2000
+
+# float = 3.14, 2.718, 0.001
+
+# str = "Hello, World!", "Python", "Programming"
+
+# bool = True, False
+
+
+rank = 10
+friend_name = "Guna"
+balance = 1000.50
+is_active = True
+
+
+print(type(rank))
+print(type(friend_name))
+print(type(balance))
+print(type(is_active))
