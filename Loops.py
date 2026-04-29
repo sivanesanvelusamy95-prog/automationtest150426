5fd5ce8c91f2c3dea7d7bfcd5cd88412b9734446
sivanesanvelusamy95-prog <sivanesanvelusamy95@gmail.com>
2026-04-29 18:39:48 +0530

    loopstopic

---
+++ Loops.py
@@ -0,0 +1,28 @@
+for i in range(5):
+	   print(i)
+
+#printnumbers
+for i in range(1,6):
+   print(i)
+
+#rangefunction
+for i in range(0,10,2):
+   print(i)
+ 
+#while
+i = 1
+while i <= 5:
+   print(i)
+   i += 1
+
+ #break
+ for i in range(7):
+  if i == 5:
+       Break
+   print(i)
+  
+  #continue
+  for i in range(6):
+   if i == 3:
+       Continue
+   print(i)
