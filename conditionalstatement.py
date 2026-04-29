48e57f7e94a777c8a739b64b356c2f579f246ea8
sivanesanvelusamy95-prog <sivanesanvelusamy95@gmail.com>
2026-04-29 18:45:19 +0530

    conditionalstatementtopic

---
+++ conditionalstatement.py
@@ -0,0 +1,31 @@
+#if
+age = 18
+if age >= 18:
+   		print("You are eligible to vote")
+
+#ifelse
+age = 16
+if age >= 18:
+ print("Eligible to vote")
+else:
+ print("Not eligible to vote")
+
+#if-else-elsefi
+marks = 75
+if marks >= 90:
+   print("Grade A")
+elif marks >= 60:
+   print("Grade B")
+else:
+   print("Grade C")
+
+#and
+age = 20
+if age >= 18 and age <= 60:
+   print("Working age")
+
+#nestedif
+age = 25
+if age >= 18:
+   if age < 60:
+       print("Eligible adult")
